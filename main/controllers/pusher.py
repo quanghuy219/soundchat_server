529fdff23aacde216d43bb6906d06f8ccdab57b3
Huy Pham <p.q.huy97@gmail.com>
2019-04-30 16:24:38 +0700

    Handle user subscribes and unsubscribe channel event

--- main/controllers/pusher.py
+++ main/controllers/pusher.py
@@ -3,10 +3,11 @@ from flask import request, jsonify
 from main import app, db
 from main.models.room import Room
 from main.models.room_paticipant import RoomParticipant
+from main.models.user import User
 from main.utils.helpers import access_token_required
-from main.libs.pusher import authenticate, read_pusher_webhook, parse_channel_name
+from main.libs.pusher import authenticate, read_pusher_webhook, parse_channel_name, trigger
 from main.errors import Error
-from main.enums import MediaStatus, ParticipantStatus
+from main.enums import MediaStatus, ParticipantStatus, PusherEvent
 
 
 @app.route('/pusher/auth', methods=['POST'])
@@ -29,6 +30,8 @@ def pusher_webhook():
             _handle_channel_vacated(event)
         elif event['name'] == 'member_removed':
             _handle_member_removed(event)
+        elif event['name'] == 'member_added':
+            _handle_member_added(event)
     return 'ok'
 
 
@@ -46,13 +49,41 @@ def _handle_member_removed(data):
     channel_name = data['channel']
     room_id = parse_channel_name(channel_name)
     user_id = data['user_id']
+    user = User.query.filter_by(id=user_id).one()
+    data = {
+        'user_id': user.id,
+        'name': user.name,
+        'email': user.email
+    }
+    trigger(room_id, PusherEvent.EXIT_PARTICIPANT, data)
 
     participant = RoomParticipant.query \
         .filter(RoomParticipant.user_id == user_id) \
         .filter(RoomParticipant.room_id == room_id) \
-        .one()
+        .one_or_none()
 
-    participant.status = ParticipantStatus.OUT
-    db.session.commit()
+    if participant:
+        participant.status = ParticipantStatus.OUT
+        db.session.commit()
 
 
+def _handle_member_added(data):
+    channel_name = data['channel']
+    room_id = parse_channel_name(channel_name)
+    user_id = data['user_id']
+    user = User.query.filter_by(id=user_id).one()
+    data = {
+        'user_id': user.id,
+        'name': user.name,
+        'email': user.email
+    }
+    trigger(room_id, PusherEvent.NEW_PARTICIPANT, data)
+
+    participant = RoomParticipant.query \
+        .filter(RoomParticipant.user_id == user_id) \
+        .filter(RoomParticipant.room_id == room_id) \
+        .one_or_none()
+
+    if participant:
+        participant.status = ParticipantStatus.IN
+        db.session.commit()
