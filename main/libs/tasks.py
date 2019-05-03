2c4b92f19d4b3133933b4a45cac3c2df90e44133
Huy Pham <p.q.huy97@gmail.com>
2019-05-04 00:19:00 +0700

    Set visibility timeout celery task

--- main/libs/tasks.py
+++ main/libs/tasks.py
@@ -6,3 +6,4 @@ from main.cfg import config
 
 celery_app = Celery('TaskQueue',
                     broker=config.CELERY_BROKER)
+celery_app.conf.broker_transport_options = {'visibility_timeout': 10800}
