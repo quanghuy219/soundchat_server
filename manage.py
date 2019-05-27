820567190efa3c48da9585321faa1b70eb6594d4
Huy Pham <p.q.huy97@gmail.com>
2019-05-28 02:40:39 +0700

    Add migrations files

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
 from main import app, models
@@ -9,6 +9,13 @@ from database import drop_all, create_all
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+def _make_context():
+    return dict(app=app, db=db, models=models)
+
+
+manager.add_command('shell', Shell(make_context=_make_context))
 
 
 @manager.command
